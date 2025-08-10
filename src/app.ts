d1c640ba18fc58a50df70646740b48145bd1c2d9
Marcio Marçal da Costa Garcia <marciomarcaldacostagarcia@gmail.com>
2025-08-10 17:50:03 -0300

    instalando bibliotecas para lidar com exceções

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express"
+import cors from "cors"
+import { errorHandling } from "./middlewares/error-handling"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
-app.get("/", (request, response) => response.send("Hello, World!"))
+app.use(errorHandling)
 
 export { app }
