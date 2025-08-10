3783ef2dbc4dbd2333f2e5974b7791269f57772f
Marcio Marçal da Costa Garcia <marciomarcaldacostagarcia@gmail.com>
2025-08-10 20:36:31 -0300

    validando dados da criação do usuário

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express"
+import { UserRole } from "@prisma/client"
+import { z } from "zod"
 
 class UsersController {
     async create(request: Request, response: Response) {
-        response.json({ message: "Ok" })
+        const bodySchema = z.object({
+            name: z.string().trim().min(2, { message: "Nome é obrigatório" }),
+            email: z.string().trim().email({ message: "E-mail inválido" }).toLowerCase(),
+            password: z.string().min(6, { message: "A senha deve ter pelo menos 6 dígitos" }),
+            role: z.enum([UserRole.employee, UserRole.manager]).default(UserRole.employee)
+        })
+
+        const { name, email, password, role } = bodySchema.parse(request.body)
+
+        response.json({ name, email, password, role })
     }
 }
 
