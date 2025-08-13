e2c753dfe97b88cfa9bc6bfb4fa4ccda691d82f7
Marcio Marçal da Costa Garcia <marciomarcaldacostagarcia@gmail.com>
2025-08-13 17:33:16 -0300

    mostrar as imagens pelo backend

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,14 @@ import cors from "cors"
 import { errorHandling } from "./middlewares/error-handling"
 import { routes } from "./routes"
 
+import uploadConfig from "./configs/upload"
+
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+// pesquisar
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes)
 app.use(errorHandling)
